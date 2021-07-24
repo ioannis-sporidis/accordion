5433c370a3b953d1b5a9a3c475c4f9337e0f5e3e
ioannis-sporidis <ioannis.sporidis@gmail.com>
2021-07-24 01:48:48 -0400

    Initial static elements setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { useState } from 'react';
+import data from './data';
+
 function App() {
-  return <></>;
+  const [questions, setQuestions] = useState(data);
+
+  return questions.map(question => {
+    const { id, title, info } = question;
+    return (
+      <div key={id}>
+        <h4>{title}</h4>
+        <p>{info}</p>
+      </div>
+    );
+  });
 }
 
 export default App;
